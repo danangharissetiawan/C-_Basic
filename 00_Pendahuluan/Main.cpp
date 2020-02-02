bf1a98f356690d2ceac460f6209ff13bf15d4893
danangharissetiawan <Mr.danangharissetiawan@gmail.com>
2020-02-02 23:26:21 +0700

    Belajar c++ eps 00 Pendahuluan

---
+++ 00_Pendahuluan/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << " Hallo Dunia" << std::endl;
+    return 0;
+}
+
+/*
+iostream: Input output stream header yang digunakan standar input output operasi dalam bahasa C++
+std: Standart library 
+cout: untuk menampilkan data ke layar
+endl: End line / new line, perintah pindah baris
+ */
